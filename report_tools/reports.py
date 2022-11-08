24f901ea0679f434bde1651764631e43054629bf
Steve Blamey <steve.blamey@adbury.net>
2022-11-08 15:45:02 +0000

    Replace django.SortedDict with collections.OrderedDict

--- report_tools/reports.py
+++ report_tools/reports.py
@@ -1,7 +1,7 @@
 from copy import deepcopy
+from collections import OrderedDict
 
 from django.db import models
-from django.utils.datastructures import SortedDict
 from django.utils.safestring import mark_safe
 from django.utils.encoding import force_text
 from django.utils.encoding import python_2_unicode_compatible
@@ -54,7 +54,7 @@ def get_declared_charts(bases, attrs, with_base_charts=True):
             if hasattr(base, 'declared_charts'):
                 charts = base.declared_charts.items() + charts
     
-    return SortedDict(charts)
+    return OrderedDict(charts)
 
 
 class DeclarativeChartsMetaclass(type):
